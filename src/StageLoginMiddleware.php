a5c4486b984dfd81b587c62e08eb2590edc790f6
Kieran Ashley <kieran@actinity.com>
2024-10-07 11:29:53 +0100

    Fix ref

--- src/StageLoginMiddleware.php
+++ src/StageLoginMiddleware.php
@@ -17,7 +17,7 @@ class StageLoginMiddleware
             $enabled &&
             (
                 !$request->cookie('stage_login') ||
-                $request->cookie('stage_login') !== config('stage_login.code')
+                $request->cookie('stage_login') !== config('stage-login.code')
             )
         ) {
             return response()->view('stage-login::index');
